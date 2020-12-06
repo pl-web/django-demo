850ab155ad66e111047442f2ae9327a88bbeac2c
it-reader <hnature@126.com>
2020-12-06 15:05:33 +0800

    python manage.py makemigrations

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+  name = models.CharField(max_length=30)
+  age = models.IntegerField()
+  personal_id = models.CharField(max_length=30)
