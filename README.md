cb8217a1816a7f7ca1e8c6cc1687fc690f67bc7f
it-reader <hnature@126.com>
2020-12-06 15:11:34 +0800

    admin.py

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ### 3. 在models中写入数据内容，使用python manage.py makemigrations命令进行迁移
 
 ### 4. 使用python manage.py migrate添加到数据库
+
+### 5. 在admin.py中进行注册
--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class Person(models.Model):
   name = models.CharField(max_length=30)
   age = models.IntegerField()
   personal_id = models.CharField(max_length=30)
